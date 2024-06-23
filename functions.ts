4b24321d89ddbbecd1695321efdbed6fe5494aa8
xFerrum <xferrum0@gmail.com>
2024-06-23 11:38:23 +0200

    aaaaaaaaaaaaaaaaaaa

---
+++ functions.ts
@@ -0,0 +1,18 @@
+import { Creature } from "./models/creature";
+import { Skill } from "./models/skill";
+import { User } from "./models/user";
+
+const io = require('socket.io')(3005,
+{
+    cors:
+    {
+        origin: ['https://allatos-umber.vercel.app/'],
+    }
+});
+
+let userSkillOptions = new Map<string, Skill[]>;
+
+io.on('connection', (socket: any) =>
+{
+    console.log("asd");
+});
