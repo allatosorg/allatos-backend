6a941c7a72e408fcd29ac60e0aa789433a44f0aa
xFerrum <xferrum0@gmail.com>
2024-06-23 13:07:57 +0200

    aaaaaaaaaaaaaaaaaaa

--- functions.ts
+++ functions.ts
@@ -2,7 +2,10 @@ import { Creature } from "./models/creature";
 import { Skill } from "./models/skill";
 import { User } from "./models/user";
 
-const io = require('socket.io')(3005,
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http,
 {
     cors:
     {
@@ -15,4 +18,8 @@ let userSkillOptions = new Map<string, Skill[]>;
 io.on('connection', (socket: any) =>
 {
     console.log("asd");
+});
+
+http.listen(3005, () => {
+    console.log('Server listening on port', 3005);
 });
