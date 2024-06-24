0e1201129f986e925a10d535169b66659929fca1
Márk Mészáros <mark.meszaros@inclouded.hu>
2024-06-24 15:28:42 +0200

    restore

--- functions.ts
+++ functions.ts
@@ -1,9 +1,13 @@
 import { Creature } from "./models/creature";
 import { Skill } from "./models/skill";
+import { CrService } from "./db_services/crService";
+import { generateSkill } from "./tools/skillGenerator";
+import { UserService } from "./db_services/userService";
 import { User } from "./models/user";
+import { generateCreature } from "./tools/creatureGenerator";
+import * as express from "express";
 import { createServer } from "http";
 import { Server, Socket} from "socket.io"
-import * as express from "express";
 
 const app = express();
 const server = createServer(app);
@@ -11,24 +15,91 @@ const io = new Server(server,
 {
     cors:   
     {
-        origin: '*',
-        methods: ['GET', 'POST'],
-        credentials: true 
+        origin: '*'
     }
 });
 
-let userSkillOptions = new Map<string, Skill[]>;
+let crService = new CrService;
+let userService = new UserService;
 
 io.on('connection', (socket: any) =>
 {
-    console.log("connected");
-
-    io.on('disconnect', (socket: any) =>
+    socket.on('skill-learn-requested', async (uid: string, crID: string) =>
     {
-        console.log("dc");
+        let cr = await crService.getCreatureById(crID);
+        if (uid === cr.ownedBy && cr.skillPicks.length !== 0)
+        {
+            const options = cr.skillPicks.shift();
+            socket.emit('skill-pick-ready', options);
+
+            socket.on('skill-option-selected', async (index: number) =>
+            {
+                await crService.learnSkill(crID, options[index]);
+                await crService.replaceSkillPicks(crID, cr.skillPicks);
+                socket.disconnect();
+            });
+
+            socket.on('skill-pick-skipped', async () =>
+            {
+                await crService.replaceSkillPicks(crID, cr.skillPicks);
+                socket.disconnect();
+            });
+        }
+        else socket.disconnect();
+    });
+
+    socket.on('attr-plus', async (uid: string, crID: string, which: string) =>
+    {
+        let cr = await crService.getCreatureById(crID);
+        if (cr.lvlup > 0 && cr.ownedBy === uid)
+        {        
+            switch (which)
+            {
+                case 'str':
+                    cr.str++;
+                    break;
+
+                case 'agi':
+                    cr.agi++;
+                    break;
+
+                case 'int':
+                    cr.int++;
+                    break;
+
+                default:
+                    break;
+            }
+            cr.lvlup--;
+
+            await crService.updateCreature(crID, cr);
+            socket.disconnect();
+        }
+        else socket.disconnect();
+    });
+
+    socket.on('create-creature', async (uid: string) =>
+    {
+        let user = await userService.getUser(uid);
+        if (user.ownedCreatures.length === 0)
+        {
+            let newCr = generateCreature();
+            newCr.ownedBy = uid;
+            const result = await crService.addCreature(newCr, uid);
+            if (!result)
+            {
+                console.log("Failed to create creature.");
+            }
+            else
+            {
+                await userService.registerCreature(uid, result);
+                socket.emit('creature-created');
+            }
+        }
+        socket.disconnect();
     });
 });
 
-server.listen(process.env.PORT || 3005, () => {
-    console.log('Server listening on port', process.env.PORT || 3005);
+server.listen(1200, () => {
+    console.log('Server listening on port', 1200);
 });
