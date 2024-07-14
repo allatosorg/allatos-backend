e8423ad8d7ed14b943ddf0b119d077b9c9c84c88
Márk Mészáros <xferrum0@gmail.com>
2024-07-14 15:48:38 +0200

    Update statuses.ts

--- db_services/shared/statuses.ts
+++ db_services/shared/statuses.ts
@@ -5,7 +5,7 @@ export const allStatuses = new Map<string, Status>
     [ "Weakened", new Status("Weakened", "You deal 25% less damage with attacks.", 1) ],
     [ "Pumped", new Status("Pumped", "You deal 25% more damage with attacks.", 1) ],
     [ "Vulnerable", new Status("Vulnerable", "You take 25% more damage from attacks.", 1) ],
-    [ "First", new Status("First", "You won the initiative roll, and you will be first to attack.", 1) ],
+    [ "First", new Status("First", "You won the initiative roll, and you will be first to act.", 1) ],
     [ "Fatigued",  new Status("Fatigued", "You're exhausted and need to rest. You can only play 1 card this turn and you became Vulnerable.", 1) ],
     [ "Bolstered", new Status("Bolstered", "You gain more block from cards.", 1, false) ],
     [ "Strengthened", new Status("Strengthened", "You deal more damage with attacks.", 1, false) ],
